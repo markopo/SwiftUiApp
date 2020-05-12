7299f1c9fc54bf2f8c85bbea805ca89728aaf56b
Marko <marko_poikkimaki@hotmail.com>
2020-05-12 08:35:43 +0200

    Initial Commit

---
+++ SwiftUiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiApp
+//
+//  Created by Marko Poikkimäki on 2020-05-12.
+//  Copyright © 2020 Marko Poikkimäki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
