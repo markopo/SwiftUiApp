d141fa0e21886fab2df451e58335d4530011720e
Marko <marko_poikkimaki@hotmail.com>
2020-05-12 09:40:36 +0200

    First commit

--- SwiftUiApp/ContentView.swift
+++ SwiftUiApp/ContentView.swift
@@ -9,8 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var titleColor: Color = Color.red
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+              Text("Swift UI APP is Cool!")
+                .font(.largeTitle)
+                .bold()
+            }.background(titleColor)
+             .foregroundColor(Color.white)
+             .padding()
+             .border(titleColor, width: 5)
+            
+
+            VStack {
+                Text("Placeholder")
+            }
+            .padding()
+            
+            VStack {
+                Button(action: {
+                    print("red button")
+                    self.titleColor = Color.red
+                }) {
+                      Text("Red Button")
+                      .padding()
+                      .foregroundColor(Color.red)
+                      .font(.title)
+                      .border(Color.red, width: 5)
+                }
+                Button(action: {
+                    print("green button")
+                    self.titleColor = Color.green
+                }) {
+                      Text("Green Button")
+                      .padding()
+                      .foregroundColor(Color.green)
+                      .font(.title)
+                      .border(Color.green, width: 5)
+                }
+                Button(action: {
+                    print("blue button")
+                    self.titleColor = Color.blue
+                }) {
+                      Text("Blue Button")
+                      .padding()
+                      .foregroundColor(Color.blue)
+                      .font(.title)
+                      .border(Color.blue, width: 5)
+                }
+            }
+            .background(Color.gray)
+            .padding()
+        }
     }
 }
 
